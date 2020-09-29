d4a87e0e555f3a16ddca11bea3c55c8f95c4acc0
Roman Holubkov <sergeykatelyna@gmail.com>
2020-09-29 14:14:45 +0300

    Fixed the fetch script

--- src/code/index.js
+++ src/code/index.js
@@ -1,22 +1,49 @@
 import './style.scss';
 
-fetch(
-  'https://api.giphy.com/v1/gifs/trending?api_key=D4EUK5TYyx5bYrrOo6wYGFV91OvXIH4J&limit=10',
-).then((res) => {
-  res.json().then(({ data }) => {
-    const gifContainerTag = document.querySelector('.gif-container');
 
-    const originalGifArr = data.map((gifObj) => gifObj.images.original);
+const RESOURCE = {
+  url: 'https://api.giphy.com/v1/gifs/trending',
+  apiKey: 'D4EUK5TYyx5bYrrOo6wYGFV91OvXIH4J',
+  limit: '10',
+};
 
-    const minHeight = Math.min(...originalGifArr.map((originalGif) => originalGif.height));
-    document.styleSheets[0].insertRule(`.gif-container img { height: ${minHeight}px }`, 0);
+const GIF_TYPE = 'original';
 
-    originalGifArr.forEach(({ url }) => {
-      const gifTag = document.createElement('IMG');
-      gifTag.src = url;
-      gifContainerTag.append(gifTag);
-    });
+const GIF_CONTAINER_TAG = document.querySelector('.gif-container');
 
-    console.log(originalGifArr);
+
+function setMinHeight(gifObjs, gifContainerTag) {
+  const minHeight = Math.min(...gifObjs.map((gifObj) => gifObj.height));
+
+  document.styleSheets[0].insertRule(
+    `.${gifContainerTag.classList[0]} img { height: ${minHeight}px }`,
+    0,
+  );
+}
+
+function showGifs(gifObjs, gifContainerTag = {}) {
+  setMinHeight(gifObjs, gifContainerTag);
+
+  gifObjs.forEach(({ url }) => {
+    const gifTag = document.createElement('IMG');
+    gifTag.src = url;
+    gifContainerTag.append(gifTag);
   });
-});
+}
+
+function fetchGifs(resource = {}, gifType, gifContainerTag) {
+  fetch(
+    `${resource.url}?api_key=${resource.apiKey}&limit=${resource.limit}`,
+  ).then((res) => {
+    res.json().then(({ data }) => {
+      const gifObjs = data.map((gifObj) => gifObj.images[gifType]);
+
+      showGifs(gifObjs, gifContainerTag);
+
+      console.log(gifObjs);
+    });
+  });
+}
+
+
+fetchGifs(RESOURCE, GIF_TYPE, GIF_CONTAINER_TAG);
